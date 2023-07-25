bee20f1c579233c67911857a5dcc2f18c774ce11
Simon Dobson <simon.dobson@computer.org>
2023-07-25 16:09:27 +0100

    First master

---
+++ code.py
@@ -0,0 +1,39 @@
+# Master weather station script
+#
+# Copyright (C) 2023 Simon Dobson
+#
+# This file is part of whether, a modular IoT weather station
+#
+# whether is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# whether is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with whether. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+import asyncio
+import board
+import adafruit_logging as logging
+from whether import RingBuffer, logger, DHT22, Anemometer
+
+# Set the logger
+logger.setLevel(logging.DEBUG)
+
+# Create the ring buffers1
+thbuf = RingBuffer(100)
+wsbuf = RingBuffer(100)
+
+# Create the sensors
+th = DHT22('temperature-humidity', board.GP10, thbuf, 2)
+ws = Anemometer('windspeed', board.GP17, wsbuf, 2)
+
+# Start the coroutines
+with asyncio.TaskGroup() as tg:
+    tg.create_task(th.run)
+    tg.create_task(ws.run)
