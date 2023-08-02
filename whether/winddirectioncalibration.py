d2c59c36e88f081db73d5aed68434c750c10c414
simoninireland <simoninireland@gmail.com>
2023-08-02 17:04:49 +0100

    Calibration for current set-up

---
+++ whether/winddirectioncalibration.py
@@ -0,0 +1,22 @@
+# windDirections.py: Wind direction sensor calibration values
+# Collected 2023-08-02 16:35:45.883721
+
+windDirections = dict(
+   N=9856,
+   NNE=7168,
+   NE=18496,
+   ENE=13120,
+   E=26944,
+   ESE=23616,
+   SE=48640,
+   SSE=46656,
+   S=62656,
+   SSW=57920,
+   SW=60544,
+   WSW=52032,
+   W=55552,
+   WNW=35648,
+   NW=38848,
+   NNW=8960,
+)
+windDirectionSensitivity = 0.04
